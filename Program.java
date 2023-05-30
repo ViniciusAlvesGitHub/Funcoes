017ada1d17b51b18830371229bddabd53c7fa93e
Vinicius <viniciusfull123@yahoo.com.br>
2023-05-30 20:51:08 -0300

    primeiro commit

---
+++ Program.java
@@ -0,0 +1,32 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com o valor do dolar:");
+		double valorCotacao = sc.nextDouble();
+		
+		System.out.println("Entre com o valor a ser convertido:");
+		double valorAConverter = sc.nextDouble();
+		
+		converterDolar(valorCotacao, valorAConverter);
+
+	}
+	
+	static void converterDolar (double cotacao, double valor) { 
+		double valorConvertidoParaDolar = valor / cotacao;
+		double valorConvertidoParaReal = valor * cotacao;
+		System.out.println("Valor convertido de real para dolar:" + valorConvertidoParaDolar);
+		System.out.println("Valor convertido de dolar para real:" + valorConvertidoParaReal);
+		sc.close();
+		*/
+	}
+
+}
